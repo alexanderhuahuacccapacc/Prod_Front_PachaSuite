9c27fb8cc79fd1f05ace109a65b05376636971b4
alexanderhuahuacccapacc <142243666+alexanderhuahuacccapacc@users.noreply.github.com>
2026-06-23 14:06:03 -0500

    Update production API URL in environment file

--- pachasuite-frontend/src/environments/environment.prod.ts
+++ pachasuite-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://prod-back-pachasuite.onrender.com/api'
-  apiUrl: '/api'
+  apiUrl: 'https://prod-back-pachasuite.onrender.com/api'
+  //apiUrl: '/api'
 };
