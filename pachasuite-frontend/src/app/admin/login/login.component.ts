cee88dd3e1d4cb60a1b73cdf8b04d933255b1a2b
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2026-06-27 21:53:51 -0500

    recepcion-permisos6

--- pachasuite-frontend/src/app/admin/login/login.component.ts
+++ pachasuite-frontend/src/app/admin/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent {
     if (this.authService.isLoggedIn() && this.authService.isAdmin()) {
       this.router.navigate(['/admin/dashboard']);
     } else if (this.authService.isLoggedIn() && this.authService.isRecepcionista()) {
-      this.router.navigate(['/admin/dashboard']);
+      this.router.navigate(['/recepcion/dashboard']);
     }
   }
 
@@ -43,7 +43,7 @@ export class LoginComponent {
         if (this.authService.isAdmin()) {
           this.router.navigate(['/admin/dashboard']);
         } else if (this.authService.isRecepcionista()) {
-          this.router.navigate(['/admin/dashboard']);
+          this.router.navigate(['/recepcion/dashboard']);
         } else {
           this.error.set('Tu cuenta no tiene un rol válido para acceder.');
           this.loading.set(false);
